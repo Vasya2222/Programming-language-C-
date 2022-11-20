9423f5f371ed084fbecdc2edcc8e9be4b9ff1219
Vasya Ryzhov <vasyaryzhov2906@gmail.com>
2022-11-20 17:52:48 +0300

    Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.

---
+++ Семинар 3 урок/задача 21/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 21
+
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.WriteLine("Программа по нахождению координат двух точек в 3D пространстве");
+
+Console.Write("Введите x1 -> ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите y1 -> ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите z1 -> ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите x2 -> ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите y2 -> ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите z2 ->");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+
+double x = Math.Pow(x2 - x1, 2);
+double y = Math.Pow(y2 - y1, 2);
+double z = Math.Pow(z2 - z1, 2);
+
+double ab = Math.Round(Math.Sqrt(x + y + z), 2);
+
+Console.WriteLine($"Растояние между точка А{x1}{y1}{z1} и B{x2}{y2}{z2} равно {ab}");
