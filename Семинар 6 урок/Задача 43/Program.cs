c8422137f2ccfa216d17f9e044b22dbd36808020
Vasya Ryzhov <vasyaryzhov2906@gmail.com>
2022-12-09 15:56:10 +0300

    Напишите программу, которая найдёт точку пересечения двух прямых

---
+++ Семинар 6 урок/Задача 43/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// ]заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 
+// задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.Write("Введите b1 -> ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b2 -> ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k1 -> ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k2 -> ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+if (k1 == k2){
+    Console.WriteLine("Прямые параллельны");
+}
+else{
+double x = Math.Round((b1 - b2) / (k2 - k1),2);
+double y = Math.Round(k1 * x + b1,2);
+Console.WriteLine($"Точки пересечения у прямой с координатами {b1},{k1} и прямой с координатами {b2},{k2} равняется ({x},{y})");
+}
+
+
