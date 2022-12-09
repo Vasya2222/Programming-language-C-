005e928c55232fe0a528c2d464747d1e9156a13d
Vasya Ryzhov <vasyaryzhov2906@gmail.com>
2022-12-09 17:58:10 +0300

    Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет

---
+++ Семинар 7 урок/Задача 50/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// 17 -> такого числа в массиве нет
+
+Console.WriteLine("Введите количество строк -> ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбец -> ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите индекс строки ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите индекс столбца ");
+int colums = Convert.ToInt32(Console.ReadLine());
+int[,] numbers = new int[y, x];
+FillArray2D(numbers);
+PrintArray2D(numbers);
+
+if (rows < numbers.GetLength(0) && colums < numbers.GetLength(1)) Console.WriteLine(numbers[rows, colums]);
+else Console.WriteLine($"{rows}{colums} -> такого числа в массиве нет");
+
+
+// Заполнение массива рандомными числами
+void FillArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 1000);
+        }
+    }
+}
+
+//  Функция вывода массива в терминал
+void PrintArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
